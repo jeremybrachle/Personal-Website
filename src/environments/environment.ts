e457a185d9ab56dda40bd7445bd0bfc86acf1cc4
Jeremy Brachle <jeremybrachle@gmail.com>
2018-07-15 08:37:14 -0400

    Fixed git ignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,7 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
+
 export const environment = {
   production: false,
   firebase: {
@@ -14,3 +15,4 @@ export const environment = {
     messagingSenderId: '45643017084'
   }
 };
+
